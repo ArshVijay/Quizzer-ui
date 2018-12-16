2f6c2b09b24697a96605484d0fe8d7ab79fffeb3
ArshVijay <gandhamvijaykumar@gmail.com>
2018-12-16 16:05:35 +0530

    added side navigation

--- quizzerui/src/app/app-routing.module.ts
+++ quizzerui/src/app/app-routing.module.ts
@@ -4,13 +4,17 @@ import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
 import { ContactComponent } from './contact/contact.component';
 import { QuestionSetNavComponent } from './question-set-nav/question-set-nav.component';
+import { QuestionComponent } from './question/question.component';
+import { ScoreComponent } from './score/score.component';
 
 
 const routes: Routes = [
   {path: '', component: HomeComponent},
   {path: 'about', component: AboutComponent},
   {path: 'contact', component: ContactComponent},
-  {path: 'QuestionSetNav', component: QuestionSetNavComponent}
+  {path: 'QuestionSetNav', component: QuestionSetNavComponent},
+  {path: 'Question', component: QuestionComponent},
+  {path: 'Score', component: ScoreComponent},
 ];
 
 @NgModule({
